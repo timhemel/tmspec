11e92c504dc17436ee99b33774923126cff42888
Tim Hemel <tim@securesoftware.nl>
2019-09-25 15:01:29 +0200

    ThreatAnalyzer class

---
+++ python3/ThreatAnalyzer.py
@@ -0,0 +1,12 @@
+
+class ThreatAnalyzer:
+
+    def set_model(self, model):
+        pass
+
+    def add_threat_library_from_prolog_file(self, path):
+        pass
+
+    def analyze(self):
+        pass
+

13782cb4a162d0f7cc7805dbf87f5678a568cbbd
Tim Hemel <tim@securesoftware.nl>
2021-03-12 08:16:54 +0100

    change build script so that it can work with a containerized antlr4.

--- tmspec/build.sh
+++ tmspec/build.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 GRAMMAR=tmspec
 d=`dirname "$0"`
+
 
 rm -f "$d"/${GRAMMAR}Lexer.py "$d"/${GRAMMAR}Lexer.tokens "$d"/${GRAMMAR}Listener.py "$d"/${GRAMMAR}Parser.py "$d"/${GRAMMAR}.tokens "$d"/${GRAMMAR}Visitor.py
 
 g=`realpath "$d"/${GRAMMAR}.g4`
 
-antlr4 -o "$d" -Dlanguage=Python3 -visitor "$g"
+pushd "$d"
+# antlr4 -o "$d" -Dlanguage=Python3 -visitor "$g"
+antlr4 -o "$d" -Dlanguage=Python3 -visitor "$GRAMMAR.g4"
+popd
 

3ff44b3456aaa257a331ed07215876aa66bf6300
Tim Hemel <tim@securesoftware.nl>
2021-03-17 09:07:59 +0100

    convert cli to click, refactored analysis reporting code.

--- src/tmspec/ErrorsAndQuestionsReporter.py
+++ src/tmspec/ErrorsAndQuestionsReporter.py
@@ -1,33 +1,37 @@
 
 from .ThreatAnalyzer import ThreatAnalysisError, ThreatAnalysisQuestion, ThreatAnalysisThreat
 
-class ErrorsAndQuestionsReporter:
-    def __init__(self, results, errors=True, questions=True, threats=False):
-        def make_error_line(r):
-            # filename:line:column:message
-            filename = r.get_filename()
-            line, column = r.get_position()
-            if isinstance(r, ThreatAnalysisError):
-                result_type = 'ERROR'
-            elif isinstance(r, ThreatAnalysisQuestion):
-                result_type = 'QUESTION'
-            elif isinstance(r, ThreatAnalysisThreat):
-                result_type = 'THREAT'
-            long_descr = "\n".join(['   '+l
-                for l in r.get_long_description().splitlines()])
-            message = '%s %s: %s\n%s' % (result_type, r.get_id(),
-                r.get_short_description(), long_descr)
-            return "%s:%d:%d:%s" % (filename, line, column, message)
-        items = []
-        if errors:
-            items += results.get_errors()
-        if questions:
-            items += results.get_questions()
-        if threats:
-            items += results.get_threats()
-        items = sorted(items, key=
-                lambda x: (x.get_filename(), x.get_position()))
-        self.error_report = "\n".join([make_error_line(e) for e in items])
-    def get(self):
-        return self.error_report
+def make_error_line(r):
+    # filename:line:column:message
+    filename = r.get_filename()
+    line, column = r.get_position()
+    if isinstance(r, ThreatAnalysisError):
+        result_type = 'ERROR'
+    elif isinstance(r, ThreatAnalysisQuestion):
+        result_type = 'QUESTION'
+    elif isinstance(r, ThreatAnalysisThreat):
+        result_type = 'THREAT'
+    long_descr = "\n".join(['   '+l
+        for l in r.get_long_description().splitlines()])
+    message = '%s %s: %s\n%s' % (result_type, r.get_id(),
+        r.get_short_description(), long_descr)
+    return "%s:%d:%d:%s" % (filename, line, column, message)
+
+def report_items(outf, items):
+    r_items = sorted(items, key= lambda x: (x.get_filename(), x.get_position()))
+    for i in r_items:
+        outf.write(make_error_line(i))
+        outf.write('\n')
+
+def report(results, out_file, errors=True, questions=True, threats=False, errors_file=None, questions_file=None, threats_file=None):
+    if errors:
+        outf_errors = errors_file if errors_file else out_file
+        report_items(outf_errors, results.get_errors())
+    if questions:
+        outf_questions = questions_file if questions_file else out_file
+        report_items(outf_questions, results.get_questions())
+    if threats:
+        outf_threats = threats_file if threats_file else out_file
+        report_items(outf_threats, results.get_threats())
+
 
--- src/tmspec/cli.py
+++ src/tmspec/cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import click
 import argparse
 import pathlib
 from yldprolog.engine import to_python
@@ -9,7 +10,7 @@ from .ThreatAnalyzer import ThreatAnalyzer
 from .ThreatLibrary import ThreatLibrary
 from .GraphvizDFDRenderer import *
 from .JSONThreatsReporter import JSONThreatsReporter
-from .ErrorsAndQuestionsReporter import ErrorsAndQuestionsReporter
+from . import ErrorsAndQuestionsReporter
 
 def obj_to_prolog(obj):
     if isinstance(obj, TmType):
@@ -19,6 +20,31 @@ def obj_to_prolog(obj):
     else:
         s = str(obj)
     return repr(s)
+
+def convert_specfile_to_prolog(infile, outf):
+    model = parseFile(infile)
+    a = ThreatAnalyzer()
+    a.set_model(model)
+    outf.write("%% DFD prolog code, generated from:\n")
+    outf.write(f"%% {infile}\n\n")
+    for predicate, answers in a.query_engine._predicates_store.items():
+        outf.write("%% %s/%d:\n" % predicate)
+        for answer in answers:
+            values = ",".join(obj_to_prolog(to_python(x)) for x in answer.values)
+            outf.write("%s(%s).\n" % (predicate[0], values))
+        outf.write("\n")
+
+def analyze_specfile(infile, threat_libraries):
+    model = parseFile(infile)
+    a = ThreatAnalyzer()
+    a.set_model(model)
+    for tfn in threat_libraries:
+        p = pathlib.Path(tfn)
+        t = ThreatLibrary()
+        t.from_prolog_file(p)
+        a.add_threat_library(t)
+    results = a.analyze()
+    return results
 
 class TmToolApp:
     def __init__(self):
@@ -51,7 +77,7 @@ class TmToolApp:
             model = parseFile(self.args.input)
             dot = GraphvizDFDRenderer(model).get_dot()
         except TmspecError as e:
-            print(e, file=sys.stderr)
+            click.echo(e, err=True)
 
     def _analyze_dfd(self):
         # --analyze -t threats1 -t threats2 -i <input>
@@ -95,8 +121,70 @@ class TmToolApp:
                 print("%%%% Threat library %s\n" % tfn)
                 sys.stdout.write(f.read())
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 def main():
-    TmToolApp().run()
+    pass
+    # TmToolApp().run()
+
+@main.command()
+# TODO: output results instead of showing to screen
+# @click.option('-o', '--out-file', type=click.Path(exists=False, allow_dash=True), default='-')
+@click.argument('infile', type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, allow_dash=True))
+def visualize(out_file, infile):
+    try:
+        model = parseFile(infile)
+        dot = GraphvizDFDRenderer(model).get_dot()
+    except TmspecError as e:
+        click.echo(e, err=True)
+
+
+@main.command()
+@click.option('-t', '--threat-library', 'threat_libraries', multiple=True, type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, allow_dash=True))
+@click.option('-o', '--out-file', type=click.Path(exists=False, allow_dash=True), default='-')
+@click.argument('infiles', nargs=-1, type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, allow_dash=True))
+def prolog(threat_libraries, out_file, infiles):
+    """Convert dfd spec file and threat libraries to prolog code."""
+    outf = click.open_file(out_file, "w")
+    # TODO: include built-in tmspec files
+    for infile in infiles:
+        try:
+            convert_specfile_to_prolog(infile, outf)
+        except TmspecError as e:
+            click.echo(e, err=True)
+    # TODO: include built-in libraries
+    for tfn in threat_libraries:
+        p = pathlib.Path(tfn)
+        with open(p,"r") as f:
+            outf.write(f"%%%% Threat library {tfn}\n\n")
+            outf.write(f.read())
+
+
+@main.command()
+@click.option('-t', '--threat-library', 'threat_libraries', multiple=True, type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, allow_dash=True))
+@click.option('-o', '--out-file', type=click.Path(exists=False, allow_dash=True))
+@click.argument('infiles', nargs=-1, type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=True, allow_dash=True))
+def analyze(threat_libraries, out_file, infiles):
+
+    if out_file is not None:
+        outf = click.open_file(out_file, "w")
+    else:
+        outf = sys.stderr
+
+    for infile in infiles:
+        try:
+            results = analyze_specfile(infile, threat_libraries)
+            # write threats to stdout, or to file, depending on output mode
+            # threat_report = JSONThreatsReporter(results).get()
+            # print(threat_report+'\n', file=sys.stdout)
+            report_threats = True
+            # write errors & questions to stderr
+            ErrorsAndQuestionsReporter.report(results, outf, threats=report_threats)
+            # outf.write(error_report)
+            # outf.write('\n')
+        except TmspecError as e:
+            click.echo(e, err=True)
 
 if __name__ == "__main__":
     TmToolApp().run()

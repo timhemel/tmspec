df793c89569e2e8aa3d0c7d523afac3d3fc76caa
Tim Hemel <tim@securesoftware.nl>
2021-03-17 19:01:14 +0100

    Output color reports to the console

--- src/tmspec/console_reporter.py
+++ src/tmspec/console_reporter.py
@@ -1,7 +1,47 @@
-class ConsoleReporter:
-    def __init__(self, args):
-        pass
+import click
 
-    def report(self, results, out_file, errors=True, questions=True, threats=False, errors_file=None, questions_file=None, threats_file=None):
-        pass
+def report_header():
+    click.secho('TmSpec report', fg='white', bold=True)
+    click.secho('-------------', fg='white', bold=True)
+    click.echo()
+    # showing = [ "errors" ]
+    # click.secho('Showing:')
+
+def report_error_item(i):
+    click.secho('ERROR:', fg='red', bold=True)
+    click.secho(f"   {i.get_short_description()}", fg='white', bold=True)
+    lines = "\n".join(["   " + x for x in i.get_long_description().split('\r\n')])
+    click.secho(f"\n{lines}")
+    click.echo()
+
+def report_question_item(i):
+    click.secho('QUESTION:', fg='yellow', bold=True)
+    click.secho(f"   {i.get_short_description()}", fg='white', bold=True)
+    lines = "\n".join(["   " + x for x in i.get_long_description().split('\r\n')])
+    click.secho(f"\n{lines}")
+    click.echo()
+
+def report_threat_item(i):
+    click.secho('THREAT:', fg='magenta', bold=True)
+    click.secho(f"   {i.get_short_description()}", fg='white', bold=True)
+    lines = "\n".join(["   " + x for x in i.get_long_description().split('\r\n')])
+    click.secho(f"\n{lines}")
+    click.echo()
+
+class ConsoleReporter:
+    def __init__(self, params):
+        self.params = params
+
+    def report(self, results, out_file):
+        report_header()
+        for i in results.get_errors():
+            report_error_item(i)
+        for i in results.get_questions():
+            report_question_item(i)
+        for i in results.get_threats():
+            report_threat_item(i)
+        # click.secho('Errors', fg='red')
+        # click.secho('Questions', fg='bright_yellow')
+        # click.secho('Threats', fg='magenta')
+        # click.secho('TODO: Warnings', fg='yellow')
 

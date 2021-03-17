d32c35f3be861cd0495939937bb6110075a17ca7
Tim Hemel <tim@securesoftware.nl>
2021-03-17 18:35:35 +0100

    implement JSON analysis output.

--- src/tmspec/json_reporter.py
+++ src/tmspec/json_reporter.py
@@ -1,27 +1,23 @@
 import json
 
-class JSONThreatsReporter:
-    def __init__(self, results):
-        def make_threat_issue(t):
-            return  {
-                'id': t.get_id(),
-                'elements': [ e.name for e in t.get_elements() ],
-                'short_decription': t.get_short_description(),
-                'long_decription': t.get_long_description(),
-            }
-        threats = [make_threat_issue(t) for t in results.get_threats()]
-        self.report = {
-            'version': '0.0',
-            'threats': threats,
-        }
-    def get(self):
-        return json.dumps(self.report, sort_keys=True, indent=4)
-
+def make_json_report_item(t):
+    return  {
+        'id': t.get_id(),
+        'elements': [ e.name for e in t.get_elements() ],
+        'short_decription': t.get_short_description(),
+        'long_decription': t.get_long_description(),
+    }
 
 class JsonReporter:
-    def __init__(self, args):
-        pass
+    def __init__(self, params):
+        self.params = params
 
-    def report(self, results, out_file, errors=True, questions=True, threats=False, errors_file=None, questions_file=None, threats_file=None):
-        pass
+    def report(self, results, out_file):
+        self.report = {
+            'version': '0.0',
+            'errors': [make_json_report_item(i) for i in results.get_errors()],
+            'questions': [make_json_report_item(i) for i in results.get_questions()],
+            'threats': [make_json_report_item(i) for i in results.get_threats()],
+        }
+        json.dump(self.report, out_file, sort_keys=True, indent=4)
 

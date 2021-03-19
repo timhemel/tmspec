b06e98d3821f8c56ce8f3b6c7fc4eaefdf4649a8
Tim Hemel <tim@securesoftware.nl>
2021-03-19 20:40:36 +0100

    render nested trust zones.

--- src/tmspec/graphviz_dfd_renderer.py
+++ src/tmspec/graphviz_dfd_renderer.py
@@ -15,7 +15,7 @@ class GraphvizDFDRenderer:
                         if a.startswith('gv_')]
         return dict(gv_attrs)
 
-    def _render(self):
+    def _create_graph(self):
         g = Digraph('G', engine='dot', graph_attr={
             'fontname': 'Sans',
             'fontsize': '8',
@@ -30,8 +30,32 @@ class GraphvizDFDRenderer:
             'fontname': 'Sans',
             'fontsize': '8',
         })
-        for z in self.model.get_zones() + [None]:
-            zone_components = self.model.get_zone_components(z)
+
+        return g
+
+    def _render(self):
+        graph = self._create_graph()
+
+        self._render_zones_for_zone(graph, None)
+
+        for v in self.model.get_flows():
+            edge_attrs = self._get_graphviz_attributes(v)
+            try:
+                label = v.get_attr('label')
+            except KeyError:
+                label = None
+            
+            if defined_before(v.source, v.target):
+                graph.edge(v.source.name, v.target.name, label=label, _attributes=edge_attrs)
+            else:
+                graph.edge(v.target.name, v.source.name, label=label, dir='back', _attributes=edge_attrs)
+        return graph
+
+    def _render_zones_for_zone(self, graph, zone):
+        for z in self.model.get_zones():
+            if z.get_attributes().get('zone') != zone:
+                continue
+
             if z is None:
                 zname = '???'
                 cluster_attrs = {
@@ -45,7 +69,7 @@ class GraphvizDFDRenderer:
             else:
                 zname = z.name
                 if z.get_attr('default'):
-                    print('default zone', z.get_attr('default'))
+                    # print('default zone', z.get_attr('default'))
                     cluster_attrs = {
                         'style': 'invis',
                     }
@@ -58,22 +82,13 @@ class GraphvizDFDRenderer:
                         'labeljust': 'r',
                         'label': zname,
                     }
- 
-            with g.subgraph(name='cluster_'+zname, graph_attr=cluster_attrs) as cluster:
+
+            with graph.subgraph(name='cluster_'+zname, graph_attr=cluster_attrs) as cluster:
+                zone_components = self.model.get_zone_components(z)
                 for c in zone_components:
                     node_attrs = self._get_graphviz_attributes(c)
                     cluster.node(c.name, label=c.name, _attributes=node_attrs)
 
-        for v in self.model.get_flows():
-            edge_attrs = self._get_graphviz_attributes(v)
-            try:
-                label = v.get_attr('label')
-            except KeyError:
-                label = None
-            
-            if defined_before(v.source, v.target):
-                g.edge(v.source.name, v.target.name, label=label, _attributes=edge_attrs)
-            else:
-                g.edge(v.target.name, v.source.name, label=label, dir='back', _attributes=edge_attrs)
-        return g
+                self._render_zones_for_zone(cluster, z)
+
 

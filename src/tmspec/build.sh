bf6605a6180ca7fb49b01f3dd8e6ae3e0d0b11c6
Tim Hemel <tim@securesoftware.nl>
2021-05-15 10:18:12 +0200

    remove old code

--- src/tmspec/build.sh
+++ src/tmspec/build.sh
@@ -6,10 +6,7 @@ d=`dirname "$0"`
 
 rm -f "$d"/${GRAMMAR}Lexer.py "$d"/${GRAMMAR}Lexer.tokens "$d"/${GRAMMAR}Listener.py "$d"/${GRAMMAR}Parser.py "$d"/${GRAMMAR}.tokens "$d"/${GRAMMAR}Visitor.py
 
-g=`realpath "$d"/${GRAMMAR}.g4`
-
 pushd "$d"
-# antlr4 -o "$d" -Dlanguage=Python3 -visitor "$g"
 antlr4 -o "$d" -Dlanguage=Python3 -visitor "$GRAMMAR.g4"
 popd
 
